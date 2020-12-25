1fd876333a2660a63687585e1197b616ad503e09
Contramundum <contramundum53@outlook.com>
2020-12-26 02:19:38 +0900

    spinlock

--- test.c
+++ test.c
@@ -3,33 +3,37 @@
 #include <linux/init.h>
 #include <linux/slab.h>
 #include <linux/delay.h>
+#include <linux/spinlock.h>
 #include <linux/kthread.h>
 #include <linux/sched.h>
-#include <linux/types.h>
 
-static atomic_t c;
-struct task_struct *rt, *wt;
-static int wf(void* _){
+static int c;
+DEFINE_SPINLOCK(l);
+struct task_struct* rt, *wt;
+
+static void wf(void*_){
   while(!kthread_should_stop()){
-    atomic_inc(&c);
+    spin_lock(&l);
+    c++;
+    spin_unlock(&l);
     msleep(500);
   }
   do_exit(0);
 }
-
-static int rf(void* _){
+static void rf(void*_){
   while(!kthread_should_stop()){
-    printk("cnt=%d\n", atomic_read(&c));
+    spin_lock(&l);
+    printk("c=%d\n", c);
+    spin_unlock(&l);
     msleep(500);
   }
   do_exit(0);
 }
-
 static int __init my_init(void){
   printk("Hello\n");
-  atomic_set(&c, 0);
-  rt = kthread_run(rf, NULL, "rt");
-  wt = kthread_run(wf, NULL, "wt");
+  c=0;
+  rt=kthread_run(rf, NULL, "rt");
+  wt=kthread_run(wf,NULL,"wt");
   return 0;
 }
 static void __exit my_exit(void){
@@ -37,7 +41,6 @@ static void __exit my_exit(void){
   kthread_stop(wt);
   printk("Bye\n");
 }
-
 module_init(my_init);
 module_exit(my_exit);
 MODULE_LICENSE("GPL");
