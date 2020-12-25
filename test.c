b96b551fc36bd710d74b63ff7506c1820a5ecb6c
Contramundum <contramundum53@outlook.com>
2020-12-26 02:30:20 +0900

    rwlock

--- test.c
+++ test.c
@@ -8,23 +8,23 @@
 #include <linux/sched.h>
 
 static int c;
-DEFINE_SPINLOCK(l);
-struct task_struct* rt, *wt;
+DEFINE_RWLOCK(l);
+struct task_struct* rt1, *rt2, *rt3, *wt;
 
-static void wf(void*_){
+static int wf(void*_){
   while(!kthread_should_stop()){
-    spin_lock(&l);
+    write_lock(&l);
     c++;
-    spin_unlock(&l);
+    write_unlock(&l);
     msleep(500);
   }
   do_exit(0);
 }
-static void rf(void*_){
+static int rf(void*_){
   while(!kthread_should_stop()){
-    spin_lock(&l);
-    printk("c=%d\n", c);
-    spin_unlock(&l);
+    read_lock(&l);
+    printk("c=%d\n",c);
+    read_unlock(&l);
     msleep(500);
   }
   do_exit(0);
@@ -32,12 +32,16 @@ static void rf(void*_){
 static int __init my_init(void){
   printk("Hello\n");
   c=0;
-  rt=kthread_run(rf, NULL, "rt");
+  rt1=kthread_run(rf,NULL,"rt1");
+  rt2=kthread_run(rf,NULL,"rt2");
+  rt3=kthread_run(rf,NULL,"rt3");
   wt=kthread_run(wf,NULL,"wt");
   return 0;
 }
 static void __exit my_exit(void){
-  kthread_stop(rt);
+  kthread_stop(rt1);
+  kthread_stop(rt2);
+  kthread_stop(rt3);
   kthread_stop(wt);
   printk("Bye\n");
 }
