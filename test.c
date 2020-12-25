d3c12c557471bf001e91011545614aa63a4678c2
Contramundum <contramundum53@outlook.com>
2020-12-26 02:18:35 +0900

    atomic

--- test.c
+++ test.c
@@ -1,40 +1,43 @@
-#include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/slab.h>
-#include <linux/string.h>
-#include <linux/gfp.h>
-#include <linux/highmem.h>
+#include <linux/delay.h>
+#include <linux/kthread.h>
+#include <linux/sched.h>
+#include <linux/types.h>
 
-static int __init test_init(void)
-{
-  printk(KERN_INFO "hoge: Hello\n");
-  const char* str = "48206105";
-  struct page* my_page = alloc_page(GFP_HIGHUSER);
-  if(my_page == NULL){
-    printk(KERN_INFO "hoge: No memory\n");
-    return -1;
+static atomic_t c;
+struct task_struct *rt, *wt;
+static int wf(void* _){
+  while(!kthread_should_stop()){
+    atomic_inc(&c);
+    msleep(500);
   }
-  
-  char* dest = (char*)kmap(my_page);
-  
-  strcpy(dest,str);
-  
-  printk(KERN_INFO "%s\n",dest);
+  do_exit(0);
+}
 
-  kunmap(my_page);
-  __free_page(my_page);
-  
+static int rf(void* _){
+  while(!kthread_should_stop()){
+    printk("cnt=%d\n", atomic_read(&c));
+    msleep(500);
+  }
+  do_exit(0);
+}
+
+static int __init my_init(void){
+  printk("Hello\n");
+  atomic_set(&c, 0);
+  rt = kthread_run(rf, NULL, "rt");
+  wt = kthread_run(wf, NULL, "wt");
   return 0;
 }
-
-static void __exit test_exit(void)
-{
-  printk(KERN_INFO "hoge: BYE!\n");
+static void __exit my_exit(void){
+  kthread_stop(rt);
+  kthread_stop(wt);
+  printk("Bye\n");
 }
 
-module_init(test_init);
-module_exit(test_exit);
-
-MODULE_AUTHOR("qwerty");
-MODULE_LICENSE("GPL v2");
+module_init(my_init);
+module_exit(my_exit);
+MODULE_LICENSE("GPL");
